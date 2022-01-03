1711808e3820c3d444aca527e6957962c41ade0c
ChanheeKim <cgim971@gmail.com>
2022-01-03 11:21:18 +0900

    feat: Building Placement UI
    btnTemplate을 만들어 버튼을 눌러 어떤 오브젝트를 복사할 것인지를 정하고
    버튼을 누를 때는 생성되지 않게 만듬
    
    BuildingTypeSelectUI / BuildingManager

--- Assets/Scripts/BuildingManager.cs
+++ Assets/Scripts/BuildingManager.cs
@@ -1,22 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class BuildingManager : MonoBehaviour
 {
 
+    public static BuildingManager Instance;
+
     private Camera mainCamera; //카메라 선언 why 카메라를 하나만 쓰지 않을 수도 있다
     private BuildingTypeListSO buildingTypeList;
-    private BuildingTypeSO buildingType; //BuildingTypeSO 타입을 buildingType로 선언
+    private BuildingTypeSO activeBuildingType; //BuildingTypeSO 타입을 buildingType로 선언
 
     private void Awake()
     {
+        Instance = this;
+
         buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
         //이름이 타입명과 같다면 typeof를 이용하여 불러올 수 있다. typeof(BuildingTypeListSO).Name은 BuildingTypeListSO를 불러옴
         //buildingTypeList에 값을 넣어줌 
-
-        buildingType = buildingTypeList.list[0];
-
     }
 
     private void Start()
@@ -30,31 +32,39 @@ public class BuildingManager : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        //EventSystem.current.IsPointerOverGameObject() = 포인터가 EventSystem의 위에 있는지 확인
         {
             //Instantiate(transform , vector3, quaternion)
-
-            Instantiate(buildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
+            if(activeBuildingType != null)
+            //activeBuildingType이 널이 아니면 실행
+            { 
+            Instantiate(activeBuildingType.prefab, GetMouseWorldPosition(), Quaternion.identity);
             //buildingType.prefab은 scriptableObject로 생성한 BuildingTypeSo의 오브젝트 중 선택된 오브젝트의 prefab
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            buildingType = buildingTypeList.list[0];
-        }
-
-        if (Input.GetKeyDown(KeyCode.Y))
-        {
-            buildingType = buildingTypeList.list[1];
+            }
         }
     }
 
     private Vector3 GetMouseWorldPosition()
     {
         Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        //Camera.Main.ScreenToWorldPoint를 이용하여 마우스 위치를 가져옴
         mouseWorldPosition.z = 0f;
         return mouseWorldPosition;
     }
 
+    public void SetActiveBuildingType(BuildingTypeSO buildingType)
+    {
+        activeBuildingType = buildingType;
+        //activeBuildingType에 buildingType을 넣음
+        //arrowBtn이면 null을 받음
+    }
+
+    public BuildingTypeSO GetActiveBuildingType()
+    {
+        return activeBuildingType;
+        //activeBuildingType을 반환
+
+    }
 
 }
---
+++ Assets/Scripts/BuildingTypeSelectUI.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BuildingTypeSelectUI : MonoBehaviour
+{
+
+    [SerializeField] private Sprite arrowSprite;
+
+    private Dictionary<BuildingTypeSO, Transform> btnTransformDictionary;
+
+    private Transform arrowBtn;
+
+    private void Awake()
+    {
+        Transform btnTemplate = transform.Find("btnTemplate");
+        btnTemplate.gameObject.SetActive(false);
+
+        BuildingTypeListSO buildingTypeList = Resources.Load<BuildingTypeListSO>(typeof(BuildingTypeListSO).Name);
+
+        btnTransformDictionary = new Dictionary<BuildingTypeSO, Transform>();
+
+        int index = 0;
+
+
+
+        arrowBtn = Instantiate(btnTemplate, transform);
+        arrowBtn.gameObject.SetActive(true);
+
+        float offsetAmount = 130f;
+        arrowBtn.GetComponent<RectTransform>().anchoredPosition = new Vector2(offsetAmount * index, 0);
+
+        arrowBtn.Find("image").GetComponent<Image>().sprite = arrowSprite;
+        //image라는 Transform을 찾고 Image 컴포넌트를 찾고 sprite를 buildingType의 sprite로 변경
+
+        arrowBtn.GetComponent<Button>().onClick.AddListener(() =>
+        {
+            BuildingManager.Instance.SetActiveBuildingType(null);
+            //BuildingManager의 SetActiveBuildingType 함수를 실행하며 매개변수로 buildingType을 보냄
+        });
+
+        index++;
+        //선택되지 않은 것을 만들기 위하여 arrow를 만듬
+
+
+
+        foreach (BuildingTypeSO buildingType in buildingTypeList.list)
+        {
+            Transform btnTransform = Instantiate(btnTemplate, transform);
+            btnTransform.gameObject.SetActive(true);
+
+            offsetAmount = 130f;
+            btnTransform.GetComponent<RectTransform>().anchoredPosition = new Vector2(offsetAmount * index, 0);
+
+            btnTransform.Find("image").GetComponent<Image>().sprite = buildingType.sprite;
+            //image라는 Transform을 찾고 Image 컴포넌트를 찾고 sprite를 buildingType의 sprite로 변경
+
+            btnTransform.GetComponent<Button>().onClick.AddListener(() =>
+            {
+                BuildingManager.Instance.SetActiveBuildingType(buildingType);
+                //BuildingManager의 SetActiveBuildingType 함수를 실행하며 매개변수로 buildingType을 보냄
+            });
+
+            btnTransformDictionary[buildingType] = btnTransform;
+
+            index++;
+        }
+    }
+
+    private void Update()
+    {
+        UpdateActiveBuildingTypeButton();
+    }
+
+    private void UpdateActiveBuildingTypeButton()
+    {
+
+        arrowBtn.Find("selected").gameObject.SetActive(false);
+
+        foreach (BuildingTypeSO buildingType in btnTransformDictionary.Keys)
+        //btnTransformDictionary의 Keys만큼 반복됨
+        {
+            Transform btnTransform = btnTransformDictionary[buildingType];
+            btnTransform.Find("selected").gameObject.SetActive(false);
+            //모든 selected를 SetActive로 끔
+        }
+
+        BuildingTypeSO activeBuildingType = BuildingManager.Instance.GetActiveBuildingType();
+        //지금 선택된 activeBuildingType을 가져와
+        if (activeBuildingType == null)
+        {
+            arrowBtn.Find("selected").gameObject.SetActive(true);
+        }
+        else
+        {
+            btnTransformDictionary[activeBuildingType].Find("selected").gameObject.SetActive(true);
+            //SetActive를 킴
+        }
+    }
+
+}
