c8eeac11d0121ae881aac42d2eec9023d789045e
ChanheeKim <cgim971@gmail.com>
2022-03-06 23:21:13 +0900

    feat : Hitting Solid Objects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,19 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed;
     private Vector2 moveInput;
 
+    public Rigidbody2D theRB;
+
+
 
     private void Update()
     {
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
 
-        transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0);
+        //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0);
+
+        theRB.velocity = moveInput * moveSpeed;
+
     }
 
 
