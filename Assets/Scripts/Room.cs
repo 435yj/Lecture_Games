ecc306d9bfa4a56ed0ae689f67bcac0c1e9c481c
ChanheeKim <cgim971@gmail.com>
2022-03-17 12:33:44 +0900

    feat : Making Doors

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,13 +5,25 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
 
+    public bool closeWhenEntered;
+
+    public GameObject[] doors;
+
+
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
-
         if (other.tag == "Player")
         {
             CameraController.instance.ChangeTarget(transform);
+
+            if (closeWhenEntered)
+            {
+                foreach(GameObject door in doors)
+                {
+                    door.SetActive(true);
+                }
+            }
         }
     }
 
