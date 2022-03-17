c72abcc7415ebc9cb3ae6b18a0b15ad1d4b6414c
ChanheeKim <cgim971@gmail.com>
2022-03-17 19:24:36 +0900

    Clearing A Room

--- Assets/Scripts/Room.cs
+++ Assets/Scripts/Room.cs
@@ -5,11 +5,37 @@ using UnityEngine;
 public class Room : MonoBehaviour
 {
 
-    public bool closeWhenEntered;
+    public bool closeWhenEntered, openWhenEnemiesCleared;
 
     public GameObject[] doors;
 
+    public List<GameObject> enemies = new List<GameObject>();
 
+    private bool roomActive;
+
+    private void Update()
+    {
+        if (enemies.Count > 0 && roomActive && openWhenEnemiesCleared)
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                if (enemies[i] == null)
+                {
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
+            if(enemies.Count == 0)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(false);
+
+                    closeWhenEntered = false;
+                }
+            }
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -19,11 +45,21 @@ public class Room : MonoBehaviour
 
             if (closeWhenEntered)
             {
-                foreach(GameObject door in doors)
+                foreach (GameObject door in doors)
                 {
                     door.SetActive(true);
                 }
             }
+
+            roomActive = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            roomActive = false;
         }
     }
 
